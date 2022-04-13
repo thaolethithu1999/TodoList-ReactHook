a50df9cf2b1ea2ae83608616aee5575eb8a6866b
thaolethithu1999 <thao.lethithu2502@gmail.com>
2022-04-13 17:34:46 +0700

    completed

---
+++ src/components/List.js
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import ListItem from "./ListItem";
+import { DataContext } from "./DataProvider";
+
+function List() {
+  const [todos, setTodos] = useContext(DataContext);
+
+  const switchComplete = (id) => {
+    // alert(id);
+    const newTodos = [...todos];
+    newTodos.forEach((todo, index) => {
+      if (index === id) {
+        todo.complete = !todo.complete;
+      }
+    });
+    setTodos(newTodos)
+  };
+
+  const handleEditTodos = (editvalue, id) =>{
+    const newTodos = [...todos]
+    newTodos.forEach((todo, index) =>{
+      if(index === id){
+        todo.name = editvalue
+      }
+    })
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <ul>
+        {todos.map((todo, index) => (
+          <ListItem
+            todo={todo}
+            key={index}
+            id={index}
+            checkComplete={switchComplete}
+            handleEditTodos={handleEditTodos}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default List;
